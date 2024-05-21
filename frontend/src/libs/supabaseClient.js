85098a60c6f99852526e5604e2706b11cddfd335
ValentinaRamirezGonzalez <tajaramirez@gmail.com>
2024-05-21 00:43:33 -0500

    agg backend

---
+++ frontend/src/libs/supabaseClient.js
@@ -0,0 +1,8 @@
+// import { createClient } from '@supabase/supabase-js';
+
+
+// const supabaseUrl = 'https://rxdyyrptyaliyrwenrsx.supabase.co';
+// const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4ZHl5cnB0eWFsaXlyd2VucnN4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYxMzgxNzQsImV4cCI6MjAzMTcxNDE3NH0.hOe-EdN3bbj8smhDoJuogm2X2_7Rz9qGcgv-LpTuHis";
+// const supabase = createClient(supabaseUrl, supabaseKey);
+
+// export default supabase;
